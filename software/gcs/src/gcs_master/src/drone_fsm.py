04d5452b8497a00e25b3695b288c90cc4d86cc7b
Gilsaa <gilsaa@live.dk>
2018-10-25 02:12:06 -0700

    FSM

--- software/gcs/src/gcs_master/src/drone_fsm.py
+++ software/gcs/src/gcs_master/src/drone_fsm.py
@@ -15,25 +15,27 @@ class DroneFSM():
         """
         # FSM flags. Outputs
         self.ARMED = False
+        self.READY = False
         self.TAKE_OFF = False
         self.LAND = False
         self.FLY = False
         self.CALCULATE_PATH = False
         self.EMERGENCY_LANDING = False
+
         # Drone parameters. FSM inputs
-        self.height = 0				        # Altitude
+        self.altitude = 0				        # Altitude
         self.position = [None, None]	 	# Current position
         self.destination = [None, None]	 	# Next waypoint
-        self.route = [] 			        # Entire path
-        self.distance_to_station = 0	    # Remaining distance?
-        self.ready = False
+        self.ack = False
         self.armed = False			        # Armed / Disarmed
         self.batt_ok = False			    # Battery status
         self.comm_ok = False			    # Comlink status
-        self.on_air = False			        # Whether it is in the air?
-        self.new_waypoint = False		    # ?? If new waypoint is available?
-        self.new_path = False			    # ?? If new path is available?
-        self.max_lowbatt_distance = max_lowbatt_distance
+        self.on_air = False			        # Whether it is in the air
+
+        # Path related vars
+        self.new_path = False			    # If new path is available
+        self.new_waypoint = False		    # If new waypoint is available
+
         # FSM parameters
         self.__state = "start"
         self.msg = 0            # Message type, route, waypoint, position etc.
@@ -44,44 +46,44 @@ class DroneFSM():
         """
         # Check for errors first!
         if self.msg._connection_header["topic"] == "/mavlink/drone/error":
-            if self.msg == "BATTERY_ERROR":                  # Battery error
+            if self.msg == "BATTERY_ERROR":                                         # Battery error
                 self.__state = "emergency_landing"
 
-            elif self.msg == "RC_LINK_LOSS":                 # Commlink error
+            elif self.msg == "RC_LINK_LOSS":                                        # Commlink error
                 self.__state = "recover_comm"
 
         # START state
         if self.__state == "start":
-            if self.msg._connection_header["topic"] == "/path":     # If message comes from path topic, read route
-                self.route = self.msg           		            # Read route
+            if self.msg._connection_header["topic"] == "/path":                     # If message comes from path topic, read route
+                self.route = self.msg           		                            # Read route
 
-            elif self.msg._connection_header["topic"] == "/mavlink/drone/ack":	 # Reponse from arm request
+            elif self.msg._connection_header["topic"] == "/mavlink/drone/ack":	    # Response from arm request
                 if self.msg == 1:
-                    self.msg = 0  		  # Clear message before next state.
-                    self.__state = "armed"        # Proceed to next state.
+                    self.msg = 0  		                                            # Clear message before next state.
+                    self.__state = "armed"                                          # Proceed to next state.
 
         # ARMED state
         elif self.__state == "armed":	
-            if self.msg._connection_header["topic"] == "/mavlink/drone/ack":     # If MavLink acknowledges, proceed to next state
+            if self.msg._connection_header["topic"] == "/mavlink/drone/ack":        # If MavLink acknowledges, proceed to next state
                 if self.msg == 1:
-                    self.msg = 0		# Clear msg before next state.
+                    self.msg = 0		                                            # Clear msg before next state.
                     self.__state = "taking_off"
 
         # TAKING OFF state
         elif self.__state == "taking_off":
-            if self.msg._connection_header["topic"] == "/mavlink/drone/ack":             # HOW TO DETECT TAKEOFF?
+            if self.msg._connection_header["topic"] == "/mavlink/drone/ack":        # HOW TO DETECT TAKEOFF?
                 if self.msg == 1: 
                     self.msg = 0
                     self.__state = "flying"
 
         # FLYING state
         elif self.__state == "flying":
-            if self.msg._connection_header["topic"] == "/mavlink/drone/ack":   # If destination is reached, what message is sent?
+            if self.msg._connection_header["topic"] == "/mavlink/drone/ack":        # If destination is reached, what message is sent?
                 self.msg = 0
                 self.__state = "landing"
 
-            elif self.msg._connection_header["topic"] == "/path":  # If message comes from path topic, read route
-                self.route = self.msg    # Read route
+            elif self.msg._connection_header["topic"] == "/path":                   # If message comes from path topic, read route
+                self.route = self.msg                                               # Read route
 
         # RECOVER COMM state
         elif self.__state == "recover_comm":
@@ -120,6 +122,8 @@ class DroneFSM():
         if self.__state == "start":
             if self.route == 0: # If route is empty
                 self.CALCULATE_PATH = True
+            else:
+                self.READY = True
 
         # ARMED state
         elif self.__state == "armed":
--- software/gcs/src/gcs_master/src/main.py
+++ software/gcs/src/gcs_master/src/main.py
@@ -17,6 +17,7 @@ placed in order to avoid variable changes while evolving the states.
 import rospy
 import geometry_msgs.msg
 import std_msgs.msg
+import pymap3d as pm
 # Local libraries
 import drone_fsm
 
@@ -25,38 +26,42 @@ class GcsMasterNode():
 
     def __init__(self):
         # Subscribers configuration
-        rospy.Subscriber('path', mavlink_lora_mision_list, main, queue_size=1)
-        rospy.Subscriber('mavlink/drone/ack', mavlink_lora_command_ack, main, queue_size=1)
-        rospy.Subscriber('mavlink/drone/error', std_msgs.msg.Empty, queue_size=1)
-
-        rospy.Subscriber("/dfb/ui/destination", geometry_msgs.msg.Point, self.ui_callback, queue_size=1)
-        rospy.Subscriber("/dfb/gcs/dronelink", geometry_msgs.msg.Point, self.dronelink_callback, queue_size=1)
-        rospy.Subscriber("/dfb/gcs/path_planner", geometry_msgs.msg.Point, self.planner_callback, queue_size=1)
+        rospy.Subscriber('userlink/start', geometry_msgs.msg.Point, ui_callback, queue_size=1)
+        rospy.Subscriber('pathplanner/waypoint/receive', mavlink_lora_mision_list, planner_callback, queue_size=1)
+        rospy.Subscriber('mavlink/drone/ack', mavlink_lora_command_ack, dronelink_callback, queue_size=1)
+        rospy.Subscriber('mavlink/drone/error', mavlink_lora_statustext, dronelink_callback, queue_size=1)
+        rospy.Subscriber('mavlink/drone/position', mavlink_lora_pos, dronelink_callback, queue_size=1)
 
         # Publishers configuration
-        self.path_request = rospy.Publisher('pathreq', std_msgs.msg.Empty, queue_size=1)
-        self.pos_publisher = rospy.Publisher("dfb/gcs/master/current_position", geometry_msgs.msg.Point, queue_size=1)
+        self.waypoint_request = rospy.Publisher('pathplanner/waypoint/request', std_msgs.msg.bool, queue_size=1)
         self.mavlink_drone_arm = rospy.Publisher('mavlink/drone/arm', std_msgs.msg.bool, queue_size=1)
-        self.mavlink_drone_takeoff = rospy.Publisher('mavlink/drone/takeoff', std_msgs.msg.Empty, queue_size=1)
+        self.mavlink_drone_takeoff = rospy.Publisher('mavlink/drone/takeoff', std_msgs.msg.bool, queue_size=1)
+        self.mavlink_drone_Waypoint = rospy.Publisher('mavlink/drone/waypoint', geometry_msgs.msg.Point, queue_size=1)
 
         # Create an instance of the drone finite-state-machine class.
         self.state_machine = drone_fsm.DroneFSM()
 
     def update_flags(self):
         if self.state_machine.CALCULATE_PATH:
-            self.calc_path_pub.publish(True)
+            self.path_request.publish(True)
             self.state_machine.CALCULATE_PATH = False
 
         if self.state_machine.ARMED:
-            print("Hi")
+            self.mavlink_drone_arm.publish(True)
+            self.state_machine.ARMED = False
 
         if self.state_machine.TAKE_OFF:
-            print("Hi")
-
-        if self.state_machine.LAND:
-            print("hi")
+            self.mavlink_drone_takeoff.publish(True)
+            self.state_machine.TAKE_OFF = False
 
         if self.state_machine.FLY:
+            print("hi")
+
+        if self.state_machine.WAYPOINT_REACHED:
+            self.waypoint_request.publish(True)
+            self.state_machine.WAYPOINT_REACHED = False
+
+        if self.state_machine.LAND:
             print("hi")
 
         if self.state_machine.EMERGENCY_LANDING:
@@ -66,12 +71,32 @@ class GcsMasterNode():
         self.state_machine.destination = [data.x, data.y]
         return
 
-    def dronelink_callback(self):
+    def dronelink_callback(self, data):
         self.state_machine.batt_ok = True
         self.state_machine.comm_ok = True
+        if data._connection_header["topic"] == "/mavlink/drone/error":
+            # Differentiate between batt and comms error here.
+            self.state_machine.batt_ok = False
+            self.state_machine.comm_ok = False
+
+        elif data._connection_header["topic"] == "/mavlink/drone/ack":
+            if data[2] == 1:
+                self.state_machine.ack = True
+            else:
+                self.state_machine.ack = False
+
+        elif data._connection_header["topic"] == "/mavlink/drone/position":
+            # data[0] = header
+            # data[1] = time_usec
+            self.state_machine.position = [data[2], data[3]]
+            self.state_machine.altitude = data[4]
+            # data[5] = relative_altitude
+            # data[6] = heading
         return
 
-    def planner_callback(self):
+    def planner_callback(self, data):
+        self.state_machine.waypoint = [data[0][0], data[0][1]]
+        self.state_machine.new_waypoint = True
         return
 
     def run(self):
